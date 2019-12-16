836bc20d334df4667937aedcfe99d76858f86c88
bojiaqiu <bojiaqiu@gmail.com>
2019-12-16 14:27:59 +0800

    update
    1. 加入static資料夾
    2. 修改setting的static資料夾路徑
    3. 修改search_record裡的admin
    4. 增加search_record資料表的資料庫
    5. 新增3筆資料到search_record資料表

--- DBMgr/admin.py
+++ DBMgr/admin.py
@@ -2,15 +2,23 @@ from django.contrib import admin
 from DBMgr.models import Company
 from DBMgr.models import ConvenienceStore
 from DBMgr.models import show
+
+
 # Register your models here.
 
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('id','jobname','jobaddress')
-class ConvenienceStoreAdmin(admin.ModelAdmin):
-    list_display = ('id','convenience_class','convenience_name','convenience_address')
-class showAdmin(admin.ModelAdmin):
-    list_display = ('id','title','location','locationName','latitude','longitude')
 
-admin.site.register(Company,CompanyAdmin)
-admin.site.register(ConvenienceStore,ConvenienceStoreAdmin)
-admin.site.register(show,showAdmin)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'jobname', 'jobaddress')
+
+
+class ConvenienceStoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'convenience_class', 'convenience_name', 'convenience_address')
+
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'location', 'locationName', 'latitude', 'longitude')
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(ConvenienceStore, ConvenienceStoreAdmin)
+admin.site.register(show, ShowAdmin)
